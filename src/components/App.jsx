14f4e0cd837abd028858a2953d71bec5d2accc66
JmA515 <janderson515@yahoo.com>
2021-10-05 15:19:02 -0700

    Created TitleBar component, TitleBar.css, and App.css. Added books to the App class in App component and refactored render function.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,36 @@
 import React, {Component} from 'react';
+import './App.css';
+import TitleBar from './TitleBar/TitleBar';
 
 class App extends Component {
     constructor(props){
         super(props);
-        this.state = {};
+        this.books = [
+            {title: "Mistborn", author: "Brandon Sanderson"},
+            {title: "The Night Angel Trilogy", author: "Brent Weeks"},
+            {title: "The Black Prism", author: "Brent Weeks"}
+        ];
+        this.state = {
+            bookNumber: 1
+        };
     }
 
     render(){
         return(
-            <h1>Our React App from A Component File</h1>
+            <div className = "container-fluid">
+                <TitleBar />
+                <div className = "col-md-4">
+                    {/* Button here to move to previous book that was viewed */}
+                </div>
+                <div className = "col-md-4">
+                    {/* Display book with cover here */}
+                    <h1>{this.books[this.state.bookNumber].title}</h1>
+                    <h4>{this.books[this.state.bookNumber].author}</h4>
+                </div>
+                <div className = "col-md-4">
+                    {/* Button here to move to next book to be viewed */}
+                </div>
+            </div>
         )
     }
 }
